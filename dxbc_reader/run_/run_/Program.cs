626b79420d5be3f411986bd061af5cb1ae1e7914
tanpeixiguang <tanpeixiguangok@163.com>
2020-07-30 08:33:50 +0800

    添加一个快捷编译

---
+++ dxbc_reader/run_/run_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics; 
+
+namespace run_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string configfile = Path.GetDirectoryName(JsonPathexe);
+            Process proc = new Process();
+            string targetDir = (Directory.GetCurrentDirectory() + "\\build_ps.bat");
+            Console.Write(targetDir);
+            //proc.StartInfo.WorkingDirectory = targetDir;
+            //proc.StartInfo.FileName = "";
+            // proc.StartInfo.Arguments = string.Format("10");
+            //proc.StartInfo.UseShellExecute = true;
+            Process.Start(targetDir);
+            // proc.WaitForExit();
+        }
+    }
+}
